b865a3e277c3db3dd78431aba887d458732903ff
davebulaval <david.beauchemin.5@ulaval.ca>
2020-07-06 11:55:32 -0400

    added teams notificator and better management of error when sending message

--- notif/notificator.py
+++ notif/notificator.py
@@ -1,6 +1,9 @@
 import json
+import warnings
 from abc import ABC, abstractmethod
 from email.policy import SMTP
+from smtplib import SMTPRecipientsRefused
+from time import sleep
 
 try:
     import requests
@@ -8,7 +11,7 @@ except ImportError:
     requests = None
 
 try:
-    from fbchat import Client, Message, ThreadType
+    from fbchat import Client, Message, ThreadType, FBchatException
 except ImportError:
     Client = None
     Message = None
@@ -19,13 +22,26 @@ try:
 except ImportError:
     ChannelNotify = None
 
+try:
+    import pymsteams
+except ImportError:
+    pymsteams = None
+
 
 class Notification(ABC):
     # pylint: disable=line-too-long
     """
     Abstract class to define a notification. Force implementation of method `send_notification` and specify how to send
     a notification error.
+
+    Args:
+
+        on_error_sleep_time (int): When an error occurs for the sending of the notification, it will wait this time to
+            retry one more time. Time is in seconds.
     """
+
+    def __init__(self, on_error_sleep_time: int):
+        self.on_error_sleep_time = on_error_sleep_time
 
     @abstractmethod
     def send_notification(self, message: str) -> None:
@@ -39,7 +55,7 @@ class Notification(ABC):
 
     def send_notification_error(self, error: Exception) -> None:
         """
-        Send a notification error message through the notificator.
+        Send a notification error message through the notificator, used with the wrapper.
 
 
         Args:
@@ -48,24 +64,24 @@ class Notification(ABC):
         notification_error_message = self._parse_error(error)
         self.send_notification(message=notification_error_message)
 
+    @staticmethod
+    def _parse_error(error: Exception) -> str:
+        """
+        Format the error into a readable text.
 
-def _parse_error(error: Exception) -> str:
-    """
-    Format the error into a readable text.
+        Args:
+            error (Exception): The exception raised during the script execution.
 
-    Args:
-        error (Exception): The exception raised during the script execution.
+        Returns:
+            A formatted string base on the error message and error type.
+        """
+        error_type = type(error)
+        error_message = error.args[0]
 
-    Returns:
-        A formatted string base on the error message and error type.
-    """
-    error_type = type(error)
-    error_message = error.args[0]
+        formatted_error_message = "An error of type {} occurred. An the error message is {}".format(
+            error_type, error_message)
 
-    formatted_error_message = "An error of type {} occurred. An the error message is {}".format(
-        error_type, error_message)
-
-    return formatted_error_message
+        return formatted_error_message
 
 
 class SlackNotificator(Notification):
@@ -76,7 +92,10 @@ class SlackNotificator(Notification):
     Args:
 
         webhook_url (str): a webhook url given by Slack to post content into a channel. See
-        `here <https://api.slack.com/incoming-webhooks>`_ for more detail.
+            `here <https://api.slack.com/incoming-webhooks>`_ for more detail.
+        on_error_sleep_time (int): When an error occurs for the sending of the notification, it will wait this time to
+            retry one more time. Time is in seconds.
+            (Default value = 300)
 
     Attributes:
 
@@ -92,7 +111,8 @@ class SlackNotificator(Notification):
 
     """
 
-    def __init__(self, webhook_url: str):
+    def __init__(self, webhook_url: str, on_error_sleep_time: int = 300):
+        super().__init__(on_error_sleep_time)
         if requests is None:
             raise ImportError("package requests need to be installed to use this class.")
         self.webhook_url = webhook_url
@@ -110,7 +130,15 @@ class SlackNotificator(Notification):
         try:
             requests.post(self.webhook_url, data=json.dumps(payload_message), headers=self.headers)
         except requests.exceptions.HTTPError:
-            pass
+            warnings.warn(f"Error when trying to send notification. Will retry in {self.on_error_sleep_time} seconds.",
+                          Warning)
+            sleep(self.on_error_sleep_time)
+            try:
+                requests.post(self.webhook_url, data=json.dumps(payload_message), headers=self.headers)
+            except requests.exceptions.HTTPError:
+                warnings.warn(
+                    "Second error when trying to send notification, will abort.", Warning)
+                pass
 
 
 class EmailNotificator(Notification):
@@ -158,13 +186,17 @@ class EmailNotificator(Notification):
 
     """
 
-    def __init__(self, sender_email: str, sender_login_credential: str, destination_email: str, smtp_server: SMTP):
+    def __init__(self, sender_email: str, sender_login_credential: str, destination_email: str, smtp_server: SMTP,
+                 on_error_sleep_time: int):
+        super().__init__(on_error_sleep_time)
         self.sender_email = sender_email
         self.sender_login_credential = sender_login_credential
         self.destination_email = destination_email
         self.smtp_server = smtp_server
 
-    def send_notification(self, message):
+        self._validate_login_credential()
+
+    def send_notification(self, message: str):
         """
         Send a notification message to the destination email.
 
@@ -173,16 +205,37 @@ class EmailNotificator(Notification):
             message (str): The message of the email.
 
         """
-        self.smtp_server.ehlo()
+
+        self.smtp_server.helo()
         self.smtp_server.starttls()
 
         self.smtp_server.login(self.sender_email, self.sender_login_credential)
 
         subject = "Python script notification email"
         content = 'Subject: %s\n\n%s' % (subject, message)
+
         self.smtp_server.sendmail(self.sender_email, self.destination_email, content)
 
-        self.smtp_server.close()
+        try:
+            self.smtp_server.sendmail(self.sender_email, self.destination_email, content)
+        except SMTPRecipientsRefused:
+            warnings.warn(f"Error when trying to send notification. Will retry in {self.on_error_sleep_time} seconds.",
+                          Warning)
+            sleep(self.on_error_sleep_time)
+            try:
+                self.smtp_server.sendmail(self.sender_email, self.destination_email, content)
+            except SMTPRecipientsRefused:
+                warnings.warn(
+                    "Second error when trying to send notification, will abort.", Warning)
+                pass
+        finally:
+            self.smtp_server.close()
+
+    def _validate_login_credential(self):
+        self.smtp_server.helo()
+        self.smtp_server.starttls()
+
+        self.smtp_server.login(self.sender_email, self.sender_login_credential)
 
 
 class ChannelNotificator(Notification):
@@ -208,7 +261,8 @@ class ChannelNotificator(Notification):
 
     """
 
-    def __init__(self, channel_url: str):
+    def __init__(self, channel_url: str, on_error_sleep_time: int):
+        super().__init__(on_error_sleep_time)
         if ChannelNotify is None:
             raise ImportError("package notify_run need to be installed to use this class.")
         self.notifier = ChannelNotify(endpoint=channel_url)
@@ -221,13 +275,24 @@ class ChannelNotificator(Notification):
             message (str): The message to send as a notification message to the channel.
 
         """
-        self.notifier.send(message)
+        try:
+            self.notifier.send(message)
+        except requests.exceptions.HTTPError:
+            warnings.warn(f"Error when trying to send notification. Will retry in {self.on_error_sleep_time} seconds.",
+                          Warning)
+            sleep(self.on_error_sleep_time)
+            try:
+                self.notifier.send(message)
+            except requests.exceptions.HTTPError:
+                warnings.warn(
+                    "Second error when trying to send notification, will abort.", Warning)
+                pass
 
 
 class FacebookMessengerNotificator(Notification):
     # pylint: disable=line-too-long
     """
-    Wrapper around fbchat to send a notification threw Facebook messenger to yourself.
+    Wrapper around fbchat to send a notification through Facebook messenger to yourself.
 
     Args:
 
@@ -247,7 +312,8 @@ class FacebookMessengerNotificator(Notification):
 
     """
 
-    def __init__(self, email_logging: str, logging_credential: str):
+    def __init__(self, email_logging: str, logging_credential: str, on_error_sleep_time: int):
+        super().__init__(on_error_sleep_time)
         if Client is None:
             raise ImportError("package fbchat need to be installed to use this class.")
         self.fb_client = Client(email_logging, logging_credential)
@@ -260,7 +326,72 @@ class FacebookMessengerNotificator(Notification):
             message (str): The message to send as a notification message to your Facebook messenger.
 
         """
-        self.fb_client.send(Message(text=message), thread_id=self.fb_client.uid, thread_type=ThreadType.USER)
+        try:
+            self.fb_client.send(Message(text=message), thread_id=self.fb_client.uid, thread_type=ThreadType.USER)
+        except FBchatException:
+            warnings.warn(f"Error when trying to send notification. Will retry in {self.on_error_sleep_time} seconds.",
+                          Warning)
+            sleep(self.on_error_sleep_time)
+            try:
+                self.fb_client.send(Message(text=message), thread_id=self.fb_client.uid, thread_type=ThreadType.USER)
+            except FBchatException:
+                warnings.warn(
+                    "Second error when trying to send notification, will abort.", Warning)
+                pass
 
     def __del__(self):
         self.fb_client.logout()
+
+
+class TeamsNotificator(Notification):
+    # pylint: disable=line-too-long
+    # pylint: disable=line-too-long
+    """
+    Notificator to send a notification into a Microsoft Teams channel.
+
+    Args:
+
+        webhook_url (str): a webhook url given by Microsoft Teams to post content into a channel. See
+        `here <https://docs.microsoft.com/en-us/microsoftteams/platform/webhooks-and-connectors/how-to/connectors-using>`_ for more detail.
+
+    Attributes:
+
+        teams_hook (str): The webhook url to push notification to.
+        # headers (dict): The headers of the notification.
+
+    Example:
+
+    .. code-block:: python
+
+        notif = TeamsNotificator(webhook_url="webhook_url")
+        notif.send_notification("The script is finish")
+
+    """
+
+    def __init__(self, webhook_url: str, on_error_sleep_time: int):
+        super().__init__(on_error_sleep_time)
+        if pymsteams is None:
+            raise ImportError("package pymsteams need to be installed to use this class.")
+        self.teams_hook = pymsteams.connectorcard(webhook_url)
+
+    def send_notification(self, message: str) -> None:
+        """
+        Send a notification message to the webhook url.
+
+        Args:
+            message (str): The message to send as a notification message to the webhook url.
+
+        """
+        self.teams_hook.text(message)
+        try:
+            self.teams_hook.send()
+        except pymsteams.TeamsWebhookException:
+            warnings.warn(f"Error when trying to send notification. Will retry in {self.on_error_sleep_time} seconds.",
+                          Warning)
+            sleep(self.on_error_sleep_time)
+            try:
+                self.teams_hook.text(message)
+            except FBchatException:
+                warnings.warn(
+                    "Second error when trying to send notification, will abort.", Warning)
+                pass
