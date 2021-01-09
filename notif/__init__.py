83f1ac074761a5d6716e8434ad5b3f90aed52cc4
davebulaval <david.beauchemin.5@ulaval.ca>
2021-01-09 12:13:11 -0500

    init fix

--- notif/__init__.py
+++ notif/__init__.py
@@ -1,4 +1,4 @@
 # pylint: disable=wildcard-import
-from fail_decorator import *
-from notificator import *
+from .fail_decorator import *
+from .notificator import *
 from .version import __version__
