3975946f3a8dcaff2d0c731960dac110486c729c
davebulaval <david.beauchemin.5@ulaval.ca>
2021-01-21 12:07:32 -0500

    improved setup and python version

--- setup.py
+++ setup.py
@@ -28,8 +28,10 @@ setup(
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
